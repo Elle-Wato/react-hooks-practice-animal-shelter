a1255975c5557858c2dc5baa7767edcd5a2aaca3
Wato Elle <watoelle@gmail.com>
2024-08-25 22:24:16 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,39 @@ import React, { useState } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
+
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+    // const [url, serUrl] = useState(`http://localhost:3001/pets`);
+
+  function handleChangeType(type) {
+    setFilters({ type: type });
+  }
+
+  function handleFindPetsClick(){
+    let url = "http://localhost:3000/pets";
+
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+     fetch(url)
+     .then((r)=> r.json())
+     .then((petsArray)=> {
+      setPets(petsArray);
+     });
+
+  }
+
+  function handleAdoptPet(id) {
+    const updatedPets = pets.map((pet)=> {
+      return pet.id === id ? { ...pet, isAdopted: true } : pet;
+    });
+    setPets(updatedPets);
+  }
+
+  
+
 
   return (
     <div className="ui container">
@@ -16,9 +46,14 @@ function App() {
         <div className="ui grid">
           <div className="four wide column">
             <Filters />
+            <Filters
+            onChangeType={handleChangeType}
+            onFindPetsClick={handleFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
             <PetBrowser />
+            <PetBrowser pets={pets} />
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,8 +2,19 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
+  function PetBrowser() {
   return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
-}
+ 
 
+function PetBrowser({ pets}) {
+
+
+  const petCards = pets.map((pet) => (
+   <Pet key={pet.id} pet={pet} />
+));
+  
+
+  return <div className="ui cards">{petCards}</div>;
+}
+ }
 export default PetBrowser;
